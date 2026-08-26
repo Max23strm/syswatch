b1645680b264d2a2f7b103c1a762df7cd1e210ac
Max23strm <maximiliano.ovejak@gmail.com>
2026-08-26 09:26:55 -0500

    Algunos cambios de diseño

--- frontend/src/sections/metric/index.tsx
+++ frontend/src/sections/metric/index.tsx
@@ -5,10 +5,26 @@ import Ram from './Ram'
 import Network from './Network'
 import '../../styles/metrics.css'
 
+import { useState, useEffect } from 'react';
+
 const MetricsSection = () => {
+    
+    function useViewportWidth() {
+        const [width, setWidth] = useState(window.innerWidth);
+
+        useEffect(() => {
+            const handleResize = () => setWidth(window.innerWidth);
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }, []);
+
+        return width;
+    }
+    const width = useViewportWidth();
     //TODO: CPU, RAM, disco y red
     return (
         <div className='metrics_container'>
+            <span id='medidor'>{width}</span>
             <Cpu/>
             <Disk/>
             <Ram/>
